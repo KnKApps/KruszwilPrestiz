a1c39211011791f27ce21aab8561214716c835e4
Kamil Gładyszewski <thekamg@gmail.com>
2018-06-03 10:43:14 +0200

    crash fixes and backwards compatibility

--- main/java/com/knk/kruszwilprestiz/MainActivity.java
+++ main/java/com/knk/kruszwilprestiz/MainActivity.java
@@ -1,23 +1,57 @@
 package com.knk.kruszwilprestiz;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private Map<View, Sound> soundMap =  new HashMap<>();
     private MediaPlayer mp;
+    private File fileSaveDir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getFileSaveDir();
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            // Check whether has the write settings permission or not.
+            boolean settingsCanWrite = false;
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+                settingsCanWrite = Settings.System.canWrite(this);
+            }
+
+            if(!settingsCanWrite) {
+                // If do not have write settings permission then open the Can modify system settings panel.
+                Intent intent = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS);
+                startActivity(intent);
+            }else {
+                // If has permission then show an alert dialog with message.
+                AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+                alertDialog.setMessage("Masz uprawnienia do zmiany ustawień!");
+                alertDialog.show();
+            }
+        }
         addSound(R.id.kupujetensyf, R.raw.kupujetensyf, "Kupuję ten syf, żeby Janusze dostali zawału.");
         addSound(R.id.wsadzrolexa, R.raw.wsadzrolexa,  "Wsadź w dupę Rolexa");
         addSound(R.id.bylynajdrozsze, R.raw.bylynajdrozsze,"Były najdroższe, dlatego je wziąłem");
@@ -49,7 +83,8 @@ public class MainActivity extends AppCompatActivity {
         button.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
-                return false;
+                createMenu(v);
+                return true;
             }
         });
     }
@@ -58,5 +93,94 @@ public class MainActivity extends AppCompatActivity {
         soundMap.get(view).play(this);
     }
 
+    private void createMenu(final View view) {
+        CharSequence[] options = {"POBIERZ", "WYŚLIJ MESSENGEREM", "USTAW JAKO DZWONEK", "USTAW JAKO POWIADOMIENIE", "USTAW JAKO DŹWIĘK ALARMU"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Wybierz opcję:");
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch(which) {
+                    case 0:
+                        try {
+                            soundMap.get(view).download(getApplicationContext(), fileSaveDir);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        break;
 
+                    case 1:
+                        soundMap.get(view).send(MainActivity.this, getApplicationContext(), fileSaveDir);
+                        break;
+
+                    case 2:
+                        soundMap.get(view).setAs(MainActivity.this, getApplicationContext(), fileSaveDir, Sound.Type.RINGTONE);
+                        break;
+
+                    case 3:
+                        soundMap.get(view).setAs(MainActivity.this, getApplicationContext(), fileSaveDir, Sound.Type.NOTIFICATION);
+                        break;
+
+                    case 4:
+                        soundMap.get(view).setAs(MainActivity.this, getApplicationContext(), fileSaveDir, Sound.Type.ALARM);
+                        break;
+                }
+            }
+        });
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    private void getFileSaveDir() {
+        boolean isK = true;
+        String[] permissions = {
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.CHANGE_CONFIGURATION,
+                Manifest.permission.WRITE_SETTINGS,
+                Manifest.permission.MODIFY_AUDIO_SETTINGS
+        };
+
+        for(String s: permissions) {
+            if(ContextCompat.checkSelfPermission(getApplicationContext(),s)!= PackageManager.PERMISSION_GRANTED) {
+                isK = false;
+                break;
+            }
+        }
+
+        if(isK) {
+            getSaveDir();
+        } else {
+            ActivityCompat.requestPermissions(MainActivity.this, permissions, 123);
+        }
+    }
+
+
+    private void getSaveDir() {
+        File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC), "Kruszwil Soundboard");
+        if(!file.mkdirs()) {
+            Log.i("halko", "Directory not created :c");
+        }
+
+        fileSaveDir = file;
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 123:
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getSaveDir();
+                } else {
+                    Toast.makeText(this,
+                            "Aby móc pobierać dźwięki, wysyłać je, oraz ustawiać jako dźwięk powiadomień lub dzwonka, musisz udzielić aplikacji odpowiednich uprawnień",
+                            Toast.LENGTH_LONG).show();
+                }
+                break;
+        }
+    }
 }
--- main/java/com/knk/kruszwilprestiz/Sound.java
+++ main/java/com/knk/kruszwilprestiz/Sound.java
@@ -1,28 +1,277 @@
 package com.knk.kruszwilprestiz;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.media.MediaPlayer;
-import android.view.View;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.provider.Settings;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AlertDialog;
+import android.widget.Button;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import com.facebook.messenger.MessengerUtils;
+import com.facebook.messenger.ShareToMessengerParams;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+
 
 public class Sound {
     private int soundId;
-    private View button;
+    private Button button;
     private String caption;
     private MediaPlayer mp;
 
-    public Sound(int soundId, View button, String caption, MediaPlayer mp) {
+    public enum Type {
+        NOTIFICATION, ALARM, RINGTONE
+    }
+
+    public Sound(int soundId, Button button, String caption, MediaPlayer mp) {
         this.soundId = soundId;
         this.button = button;
         this.caption = caption;
         this.mp = mp;
     }
 
-    public void play(Context context){
-        if(mp != null) {
+    public void play(Context context) {
+        if (mp != null) {
             mp.release();
             mp = null;
         }
-        mp = MediaPlayer.create(context,this.soundId);
+        mp = MediaPlayer.create(context, this.soundId);
         mp.start();
+    }
+
+    public File download(Context context, File dir) throws IOException {
+        InputStream is = null;
+        FileOutputStream fos = null;
+        File file = null;
+
+        try {
+            file = new File(dir, (String) button.getText() + ".mp3");
+            if (!file.exists() && !file.isDirectory()) {
+                file.createNewFile();
+            }
+
+            is = context.getResources().openRawResource(this.soundId);
+            fos = new FileOutputStream(file);
+
+            byte[] buffer = new byte[1024];
+            int count;
+
+            while ((count = is.read(buffer, 0, 1024)) != -1) {
+                fos.write(buffer, 0, count);
+            }
+
+            Toast.makeText(context, "Utworzono plik \n" + file.getAbsolutePath(), Toast.LENGTH_LONG).show();
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (is != null) {
+                is.close();
+            }
+            if (fos != null) {
+                fos.close();
+            }
+
+            return file;
+        }
+    }
+
+    public File download(Context context, File dir, String name) throws IOException {
+        InputStream is = null;
+        FileOutputStream fos = null;
+        File file = null;
+
+        try {
+            file = new File(dir, name + ".mp3");
+            if (!file.exists() && !file.isDirectory()) {
+                file.createNewFile();
+            }
+
+            is = context.getResources().openRawResource(this.soundId);
+            fos = new FileOutputStream(file);
+
+            byte[] buffer = new byte[1024];
+            int count;
+
+            while ((count = is.read(buffer, 0, 1024)) != -1) {
+                fos.write(buffer, 0, count);
+            }
+
+            Toast.makeText(context, "Utworzono plik \n" + file.getAbsolutePath(), Toast.LENGTH_LONG).show();
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (is != null) {
+                is.close();
+            }
+            if (fos != null) {
+                fos.close();
+            }
+
+            return file;
+        }
+    }
+
+    public File download(Context context, File dir, Type type) throws IOException {
+        InputStream is = null;
+        FileOutputStream fos = null;
+        File file = null;
+
+        try {
+            switch(type){
+                case NOTIFICATION:
+                    file = new File(dir, button.getText() + " notification.mp3");
+                    break;
+                case RINGTONE:
+                    file = new File(dir, button.getText() + " ringtone.mp3");
+                    break;
+                case ALARM:
+                    file = new File(dir, button.getText() + " alarm.mp3");
+                    break;
+            }
+
+            if (!file.exists() && !file.isDirectory()) {
+                file.createNewFile();
+            }
+
+            is = context.getResources().openRawResource(this.soundId);
+            fos = new FileOutputStream(file);
+
+            byte[] buffer = new byte[1024];
+            int count;
+
+            while ((count = is.read(buffer, 0, 1024)) != -1) {
+                fos.write(buffer, 0, count);
+            }
+
+            Toast.makeText(context, "Utworzono plik \n" + file.getAbsolutePath(), Toast.LENGTH_LONG).show();
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (is != null) {
+                is.close();
+            }
+            if (fos != null) {
+                fos.close();
+            }
+
+            return file;
+        }
+    }
+
+    public void send(Activity activity, Context context, File dir) {
+        File file;
+        try {
+            file = download(context, dir);
+            Uri uri = FileProvider.getUriForFile(context, context.getApplicationContext().getPackageName() + ".com.knk.kruszwilprestiz.provider", file);
+            ShareToMessengerParams shareToMessengerParams = ShareToMessengerParams.newBuilder(uri, "audio/mpeg").build();
+            MessengerUtils.shareToMessenger(activity, 321, shareToMessengerParams);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setAs(Activity activity, Context context, File dir, Type type) {
+
+        File file = null;
+        try {
+           switch (type) {
+                case ALARM:
+                    file = download(context, dir, Type.ALARM);
+                    break;
+
+                case RINGTONE:
+                    file = download(context, dir, Type.RINGTONE);
+                    break;
+
+                case NOTIFICATION:
+                    file = download(context, dir, Type.NOTIFICATION);
+            }
+           
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.MediaColumns.DATA, file.getAbsolutePath());
+        values.put(MediaStore.MediaColumns.SIZE, file.length());
+        values.put(MediaStore.MediaColumns.DISPLAY_NAME, file.getName());
+        values.put(MediaStore.MediaColumns.TITLE,file.getName());
+        values.put(MediaStore.MediaColumns.MIME_TYPE,"audio/mp3");
+        values.put(MediaStore.Audio.Media.ARTIST, "Kruszwil");
+        values.put(MediaStore.Audio.Media.IS_MUSIC, false);
+        values.put(MediaStore.Audio.Media.IS_RINGTONE, (type==Type.RINGTONE)? true: false);
+        values.put(MediaStore.Audio.Media.IS_NOTIFICATION, (type==Type.NOTIFICATION)? true: false);
+        values.put(MediaStore.Audio.Media.IS_ALARM, (type==Type.ALARM)? true: false);
+
+
+
+        switch (type) {
+            case RINGTONE:
+                Uri ringtoneUri = MediaStore.Audio.Media.getContentUriForPath(file.getAbsolutePath());
+
+                Uri newRingtoneUri = context.getContentResolver().insert(ringtoneUri, values);
+
+                if(newRingtoneUri == null){
+                    context.getContentResolver().delete(ringtoneUri,null,null);
+                    newRingtoneUri = context.getContentResolver().insert(ringtoneUri, values);
+                }
+                RingtoneManager.setActualDefaultRingtoneUri(context, RingtoneManager.TYPE_RINGTONE, newRingtoneUri);
+                Toast.makeText(context,"Ustawiono dzwonek",Toast.LENGTH_LONG).show();
+                break;
+
+            case NOTIFICATION:
+                Uri notificationUri = MediaStore.Audio.Media.getContentUriForPath(file.getAbsolutePath());
+
+                Uri newNotificationUri = context.getContentResolver().insert(notificationUri, values);
+
+                if(newNotificationUri == null){
+                    context.getContentResolver().delete(notificationUri,null,null);
+                    newNotificationUri = context.getContentResolver().insert(notificationUri, values);
+                }
+                RingtoneManager.setActualDefaultRingtoneUri(context, RingtoneManager.TYPE_NOTIFICATION, newNotificationUri);
+                Toast.makeText(context, "Ustawiono dźwięk powiadomień", Toast.LENGTH_LONG).show();
+                break;
+
+            case ALARM:
+                Uri alarmUri = MediaStore.Audio.Media.getContentUriForPath(file.getAbsolutePath());
+
+                Uri newAlarmUri = context.getContentResolver().insert(alarmUri, values);
+
+                if(newAlarmUri == null){
+                    context.getContentResolver().delete(alarmUri,null,null);
+                    newAlarmUri = context.getContentResolver().insert(alarmUri, values);
+                }
+                RingtoneManager.setActualDefaultRingtoneUri(context,RingtoneManager.TYPE_ALARM, newAlarmUri);
+                Toast.makeText(context, "Ustawiono dźwięk alarmu", Toast.LENGTH_LONG).show();
+                break;
+        }
     }
 }
